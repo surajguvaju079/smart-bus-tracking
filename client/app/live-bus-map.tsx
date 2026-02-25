5bf8ce85e726fa7b4139ebb4a8c0a47acff072bb
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2026-02-25 14:01:59 +0545

    poly line drawn

--- client/app/live-bus-map.tsx
+++ client/app/live-bus-map.tsx
@@ -19,6 +19,32 @@ export default function LiveBusMap() {
     if (!tripId) return;
 
     const handleLocation = (data: any) => {
+      /*   const distance = (lat1, lon1, lat2, lon2) => {
+  const R = 6371000;
+  const toRad = (v: number) => (v * Math.PI) / 180;
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+}; */
+      // setLocations(prev => {
+      //   if (prev.length === 0) return [data];
+
+      //   const last = prev[prev.length - 1];
+      //   const d = distance(last.latitude, last.longitude, data.latitude, data.longitude);
+
+      //   if (d < 5) return prev; // ignore small movements
+
+      //   return [...prev, data];
+      // });
+
       setLocations((prev) => [...prev, data]);
     };
 
